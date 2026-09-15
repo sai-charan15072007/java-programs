54cfa902899f98efc6d6519782160cdb2b57e6f6
B. Sai Charan <25q91a05ab@mrce.in>
2026-09-15 15:35:59 +0530

    Add files via upload

---
+++ NumberCheck.java
@@ -0,0 +1,17 @@
+public class NumberCheck {
+public static void main(String[] args) {
+int totalEven=0;
+int totalodd=0;
+for(int i=1;i<10;i++) {
+if (i%2==0;) {
+System.out.println(i+"is even");
+totalEven++;
+}else{
+System.out.println(i+"is odd");
+totalodd++;
+}
+}
+System.out.println("total even number from 1 to 10;"+total even);
+System.out.println("total odd number from 1 to 10;"+total odd);
+}
+}+
